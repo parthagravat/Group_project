de8f0f5f3f58c96f5031fef8074949312e9d7bc2
Ruturajsinh Gohil <gohilruturajsinh86@gmail.com>
2020-07-30 20:27:28 +0530

    Login Screen, Sign Up Screen Complete.

---
+++ app/src/main/java/com/example/group_project/Login.java
@@ -0,0 +1,36 @@
+package com.example.group_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    Button btnLogin;
+    Button btnSignup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this , AboutUs.class);
+                startActivity(intent);
+            }
+        });
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this , Signup.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
