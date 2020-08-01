d788ee584a717b9ea6c7ca55ce27bb930bb2ae91
parthagravat <parthagravat8080@gmail.com>
2020-08-01 22:20:28 +0530

    Add option Manu. change design by Ruturaj and Harshiv. Add images and delete ActionBar on splace Screen. change Name Activity_main to activity_splash. No ActionBar code in Style.xml and add to manifest file.

--- app/src/main/java/com/example/group_project/AboutUs.java
+++ app/src/main/java/com/example/group_project/AboutUs.java
@@ -1,8 +1,14 @@
 package com.example.group_project;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AboutUs extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.contact_us:
+                Intent intent = new Intent(AboutUs.this , contactUs.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/group_project/activity_splash.java
+++ app/src/main/java/com/example/group_project/activity_splash.java
@@ -6,18 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class activity_splash extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this , Login.class);
+                Intent intent = new Intent(activity_splash.this , Login.class);
                 startActivity(intent);
                 finish();
             }
